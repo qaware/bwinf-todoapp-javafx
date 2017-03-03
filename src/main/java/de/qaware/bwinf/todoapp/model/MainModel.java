f2e97e2f008129f785b0bfe92f26613708663f85
Moritz Kammerer <moritz.kammerer@qaware.de>
2017-03-03 15:51:01 +0100

    More JavaDoc.

--- src/main/java/de/qaware/bwinf/todoapp/model/MainModel.java
+++ src/main/java/de/qaware/bwinf/todoapp/model/MainModel.java
@@ -1,21 +1,28 @@
 package de.qaware.bwinf.todoapp.model;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Model of the application.
+ */
 public class MainModel {
-    private ObservableList<Entry> entries = FXCollections.observableArrayList();
+    private List<Entry> entries = new ArrayList<>();
 
-    public ObservableList<Entry> getEntries() {
+    public List<Entry> getEntries() {
         return entries;
     }
 
+    /**
+     * Loads the entries from the given file.
+     *
+     * @param file File.
+     * @throws IOException If something went wrong while loading.
+     */
     public void load(Path file) throws IOException {
         if (!Files.exists(file)) {
             return;
@@ -24,6 +31,7 @@ public class MainModel {
         List<String> lines = Files.readAllLines(file);
 
         for (String line : lines) {
+            // Format is: <progress>;<label>
             String[] parts = line.split(";", 2);
             Progress progress = Progress.valueOf(parts[0]);
             Entry entry = new Entry(progress, parts[1]);
@@ -31,6 +39,12 @@ public class MainModel {
         }
     }
 
+    /**
+     * Saves the entries to the given file.
+     *
+     * @param file File.
+     * @throws IOException If something went wrong while saving.
+     */
     public void save(Path file) throws IOException {
         List<String> lines = new LinkedList<>();
 
